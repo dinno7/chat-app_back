99b4531645c326ae361f9cd808fbe51bf2e2ef50
Dinno <tahadlrb7@gmail.com>
2024-05-18 18:14:04 +0330

    feat(redis service): extend redis service to work with lists and sets

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -32,6 +32,39 @@ export class RedisService implements OnModuleInit, OnApplicationShutdown {
   get(key: RedisKey): Promise<string> {
     return this.__redisClient.get(key);
   }
+  pushToList(key: RedisKey, ...values: (string | number | Buffer)[]) {
+    return this.__redisClient.rpush(key, ...values);
+  }
+  getList(
+    key: RedisKey,
+    startIndex: string | number = 0,
+    endIndex: string | number = -1,
+  ) {
+    return this.__redisClient.lrange(key, startIndex, endIndex);
+  }
+  getElIndexFromList(
+    key: RedisKey,
+    element: string | number | Buffer,
+  ): Promise<number | null> {
+    return this.__redisClient.lpos(key, element);
+  }
+  removeElFromList(
+    key: RedisKey,
+    element: string | number | Buffer,
+    count: number | string = 0,
+  ) {
+    //? When count is 0 it will remove all matches elements
+    return this.__redisClient.lrem(key, count, element);
+  }
+  pushToSet(key: RedisKey, ...values: (string | number | Buffer)[]) {
+    return this.__redisClient.sadd(key, ...values);
+  }
+  getSetMembers(key: RedisKey) {
+    return this.__redisClient.smembers(key);
+  }
+  removeElFromSet(key: RedisKey, ...elements: (string | number | Buffer)[]) {
+    return this.__redisClient.srem(key, ...elements);
+  }
   onModuleInit() {
     this.__redisClient = new Redis({
       host: this.redisConfiguration.host,
