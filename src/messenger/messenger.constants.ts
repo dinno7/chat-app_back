bfee3cbcb91671e7a96c0acf055f6b1f0adbba54
Dinno <tahadlrb7@gmail.com>
2024-05-18 18:16:04 +0330

    feat(messenger gateway): add connection & disconnection handlers

---
+++ src/messenger/messenger.constants.ts
@@ -0,0 +1,1 @@
+export const ONLINE_USER_REDIS_SET_KEY = 'online_users';
--- src/messenger/messenger.gateway.ts
+++ src/messenger/messenger.gateway.ts
@@ -1,12 +1,15 @@
+import { BadGatewayException } from '@nestjs/common';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { RedisService } from 'src/redis/redis.service';
 import { UserService } from 'src/user/user.service';
+import { ONLINE_USER_REDIS_SET_KEY } from './messenger.constants';
+import { MESSENGER_EMITS } from './types/emits.type';
 
 @WebSocketGateway({
   cors: {
@@ -23,4 +26,43 @@ export class MessengerService
 
   @WebSocketServer()
   server: Server;
+
+  async handleConnection(client: Socket, ...args: any[]) {
+    const user = await this.userService.getUserByJWTToken(
+      client.handshake.auth?.token,
+    );
+    if (!user?.id) throw new BadGatewayException('Token not valid!');
+
+    //? Create a room
+    client.join(user.id);
+
+    await this.redisService.pushToSet(ONLINE_USER_REDIS_SET_KEY, user.id);
+
+    this.server.emit(
+      MESSENGER_EMITS.GetOnlineUsers,
+      await this.__getAllOnlineUsers(),
+    );
+
+    console.log(`✨ new Connection `, client.id, args);
+  }
+  async handleDisconnect(client: Socket) {
+    const user = await this.userService.getUserByJWTToken(
+      client.handshake.auth?.token,
+    );
+    await this.redisService.removeElFromSet(ONLINE_USER_REDIS_SET_KEY, user.id);
+
+    this.server.emit(
+      MESSENGER_EMITS.GetOnlineUsers,
+      await this.__getAllOnlineUsers(),
+    );
+
+    console.log(`✨ `, client.id + ' Disconnected');
+  }
+  private async __getAllOnlineUsers() {
+    return [
+      ...new Set(
+        await this.redisService.getSetMembers(ONLINE_USER_REDIS_SET_KEY),
+      ),
+    ];
+  }
 }
