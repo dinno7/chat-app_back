2140dc180d7be23981882dcfd9039337df74452f
Dinno <tahadlrb7@gmail.com>
2024-05-22 17:28:47 +0330

    feat(messenger events enum): add some more

--- src/messenger/types/emits.type.ts
+++ src/messenger/types/emits.type.ts
@@ -1,3 +1,7 @@
-export enum MESSENGER_EMITS {
+export enum MESSENGER_EVENTS {
   GetOnlineUsers = 'getOnlineUsers',
+  ConsumeReceiverUserDetails = 'consumeReceiverUserDetails',
+  Message = 'message',
+  MessageAck = 'messageAck',
+  GetChatConversations = 'getChatConversations',
 }
